f82ee7b43fff652d4bda2efe5c7751598c84a4f9
Jeonwoojae <dnwo0719@icloud.com>
2023-02-10 16:18:32 +0900

    react 기본 원리를 이해하고 css 적용하기

---
+++ react-self-study/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <p>This is App</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
