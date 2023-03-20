0bc599899711f8f390af51f37888537da76cc5d0
Jeonwoojae <dnwo0719@icloud.com>
2023-03-21 00:51:16 +0900

    [feat]:입력값 받아서 useState로 저장

--- library-front/src/components/search/SearchBox.js
+++ library-front/src/components/search/SearchBox.js
@@ -1,11 +1,26 @@
-import React from "react";
+import { Button, Input } from "@mui/material";
+import React, { useState } from "react";
 import Dropdown from "./Dropdown";
 
 function SearchBox() {
-  return <div className="Box">
-    <Dropdown />
-    SearchBox
-    </div>;
+    const [text, setText] = useState('');
+
+    const onChange = (e) => {
+        setText(e.target.value);
+    }
+
+  return (
+    <div className="Box">
+      <Dropdown />
+      <Input onChange={onChange} placeholder="Placeholder"/>
+      <Button className="searchbox-button" variant="outlined" size="medium">
+        검색
+      </Button>
+      <Button className="searchbox-button" variant="outlined" size="medium">
+        추가
+      </Button>
+    </div>
+  );
 }
 
 export default SearchBox;
