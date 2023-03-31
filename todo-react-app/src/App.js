9bf2c4439a38a2a7c9f748550c2ea37fe7eeee7e
Jeonwoojae <dnwo0719@icloud.com>
2023-03-31 10:39:46 +0900

    material-ui로 리스트 렌더링하기

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,11 +1,37 @@
-import Hello from './Hello';
+import { render } from "react-dom";
+import Todo from "./Todo";
+import React from "react";
+import { Paper, List } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <Hello />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    // 매개변수 props 생성자
+    super(props); // 매개변수 props 초기화
+    this.state = {
+      // item에 item.id, item.title, item.done 매개변수 이름과 값 할당
+      items: [
+        { id: 0, title: "Todo 1", done: false },
+        { id: 1, title: "Todo 2", done: false },
+      ],
+    };
+  }
+  render() {
+    // todoItems에 length가 0보다 크다면 true
+    // 삼항연산자도 사용 가능하다
+    var todoItems = this.state.items.length > 0 && (
+      // 자바스크립트가 제공하는 map 함수를 이용해서 배열을 반복해 <Todo/> 컴포넌트를 여러 개 생성한다.
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+    );
+
+    // 생성된 컴포넌트 JSX를 리턴한다.
+    return <div className="App">{todoItems}</div>;
+  }
 }
 
 export default App;
---
+++ todo-react-app/src/Todo.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Checkbox, InputBase, ListItem, ListItemText } from "@material-ui/core";
+
+export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: props.item }; // 매개변수 item의 변수/값을 item에 대입
+  }
+  render() {
+    const item = this.state.item;
+    return (
+      <ListItem>
+        <Checkbox checked={item.done} />
+        <ListItemText>
+          <InputBase
+            inputProps={{ "aria-label": "naked" }}
+            type="text"
+            id={item.id}
+            name={item.id}
+            value={item.title}
+            multiline={true}
+            fullWidth={true}
+          />
+        </ListItemText>
+      </ListItem>
+    );
+  }
+}
