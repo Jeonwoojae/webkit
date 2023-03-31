3b04246e5c117a0b1ba6dbdaedaa5db329f15669
Jeonwoojae <dnwo0719@icloud.com>
2023-03-31 11:12:51 +0900

    이벤트 핸들러로 목록 추가

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,7 +1,8 @@
 import { render } from "react-dom";
 import Todo from "./Todo";
 import React from "react";
-import { Paper, List } from "@material-ui/core";
+import { Paper, List, Container } from "@material-ui/core";
+import AddTodo from "./AddTodo";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,11 +11,21 @@ class App extends React.Component {
     this.state = {
       // item에 item.id, item.title, item.done 매개변수 이름과 값 할당
       items: [
-        { id: 0, title: "Todo 1", done: false },
-        { id: 1, title: "Todo 2", done: false },
+        { id: "ID-0", title: "Todo 1", done: false },
+        { id: "ID-1", title: "Todo 2", done: false },
       ],
     };
   }
+  // (1) add 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id 추가
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems }); // update State
+    console.log("items:", this.state.items);
+  };
+
   render() {
     // todoItems에 length가 0보다 크다면 true
     // 삼항연산자도 사용 가능하다
@@ -30,7 +41,15 @@ class App extends React.Component {
     );
 
     // 생성된 컴포넌트 JSX를 리턴한다.
-    return <div className="App">{todoItems}</div>;
+    // (2) add 함수 연결
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
