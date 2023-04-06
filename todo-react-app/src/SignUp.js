a62c0339a13cb165ad41c1f0bd8bfc2bce3c3ebd
Jeonwoojae <dnwo0719@icloud.com>
2023-04-06 11:07:24 +0900

    로그인과 회원가입 컴포넌트 react-hook-form사용하여 함수형으로 변경

--- todo-react-app/src/SignUp.js
+++ todo-react-app/src/SignUp.js
@@ -7,94 +7,87 @@ import {
 } from "@material-ui/core";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import { signup } from "./service/ApiService";
 
-export default class SignUp extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+export default function SignUp() {
+  // 폼을 만들기 위한 여러가지 요소 불러오기
+  const { register, handleSubmit, getValues } = useForm();
 
-  handleSubmit(event) {
-    event.preventDefault();
+  // 데이터 전송시 작동할 함수 정의
+  const onValid = (data) => {
+    console.log(data);
 
-    const data = new FormData(event.target);
-    const username = data.get("username");
-    const email = data.get("email");
-    const password = data.get("password");
+    const { username, email, password } = getValues();
     signup({ email: email, username: username, password: password }).then(
       (respone) => {
         window.location.href = "/login";
       }
     );
-  }
+  };
 
-  render() {
-    return (
-      <Container component="main" maxWidth="xs" style={{ marginTop: "8%" }}>
-        <form noValidate onSubmit={this.handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Typography component="h1" variant="h5">
-                계정 생성
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete="username"
-                name="username"
-                variant="outlined"
-                required
-                fullWidth
-                id="username"
-                label="사용자 이름"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete="email"
-                name="email"
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="이메일 주소"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete="current-password"
-                name="password"
-                variant="outlined"
-                required
-                fullWidth
-                id="password"
-                label="패스워드"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                계정생성
-              </Button>
-            </Grid>
+  return (
+    <Container component="main" maxWidth="xs" style={{ marginTop: "8%" }}>
+      <form noValidate onSubmit={handleSubmit(onValid)}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography component="h1" variant="h5">
+              계정 생성
+            </Typography>
           </Grid>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link to="/login" variant="body2">
-                이미 계정이 있습니까? 로그인 하세요.
-              </Link>
-            </Grid>
+          <Grid item xs={12}>
+            <TextField
+              autoComplete="username"
+              name="username"
+              variant="outlined"
+              required
+              fullWidth
+              id="username"
+              label="사용자 이름"
+              autoFocus
+              {...register("username")}
+            />
           </Grid>
-        </form>
-      </Container>
-    );
-  }
+          <Grid item xs={12}>
+            <TextField
+              autoComplete="email"
+              name="email"
+              variant="outlined"
+              required
+              fullWidth
+              id="email"
+              label="이메일 주소"
+              autoFocus
+              {...register("email")}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              autoComplete="current-password"
+              name="password"
+              variant="outlined"
+              required
+              fullWidth
+              id="password"
+              label="패스워드"
+              autoFocus
+              {...register("password")}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              계정생성
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container justifyContent="flex-end">
+          <Grid item>
+            <Link to="/login" variant="body2">
+              이미 계정이 있습니까? 로그인 하세요.
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
 }
