79c1b2ca92f952eb52391cd5e837e29376291fda
Jeonwoojae <dnwo0719@icloud.com>
2023-04-06 15:26:09 +0900

    네비게이션에 홈버튼 구현

--- todo-react-app/src/page/Navigator.js
+++ todo-react-app/src/page/Navigator.js
@@ -1,5 +1,6 @@
 import { AppBar, Button, Grid, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { signout } from '../service/ApiService'
 
 function Navigator() {
@@ -8,8 +9,9 @@ function Navigator() {
     <AppBar position="static">
       <Toolbar>
         <Grid justifyContent="space-between" container>
-          <Grid item>
-            <Typography variant="h6">오늘의 할일</Typography>
+          <Grid item><div style={{cursor:"pointer"}} onClick={()=>window.location.href="/"}>
+          <Typography variant="h6">오늘의 할일</Typography>
+          </div>
           </Grid>
           <Grid item>
           <Button color="inherit" href="/profile">
