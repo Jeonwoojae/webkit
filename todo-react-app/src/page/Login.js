1c558f6a063a30ab4d418300dda1abab8c0cb3bf
Jeonwoojae <dnwo0719@icloud.com>
2023-04-07 14:34:45 +0900

    회원가입, 로그인 시 비밀번호 visible 모드 구현

--- todo-react-app/src/page/Login.js
+++ todo-react-app/src/page/Login.js
@@ -1,10 +1,17 @@
 import {
   Button,
   Container,
+  FormControl,
   Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
   TextField,
   Typography,
 } from "@material-ui/core";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
@@ -13,6 +20,15 @@ import { signin } from "../service/ApiService";
 function Login() {
   // 폼을 만들기 위한 여러가지 요소 불러오기
   const { register, handleSubmit, getValues } = useForm();
+
+  // 비밀번호 박스 암호화
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   const onValid = (data) => {
     console.log(data);
@@ -44,16 +60,25 @@ function Login() {
             />
           </Grid>
           <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="password"
-              label="패스워드"
-              name="password"
-              autoComplete="password"
-              {...register("password")}
-            />
+          <FormControl {...register("password")} fullWidth sx={{ m: 1 }} variant="outlined">
+              <InputLabel required >패스워드</InputLabel>
+              <OutlinedInput
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="password"
+              />
+            </FormControl>
           </Grid>
           <Grid item xs={12}>
             <Button type="submit" fullWidth variant="contained" color="primary">
