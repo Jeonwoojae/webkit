0ddb15480669a9a2e6c580257adcee02805fd0bf
Jeonwoojae <dnwo0719@icloud.com>
2023-04-14 15:39:00 +0900

    [init]server:프로젝트 생성

---
+++ recycle-market-back/src/main/java/com/example/recyclemarketback/RecycleMarketBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.recyclemarketback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleMarketBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecycleMarketBackApplication.class, args);
+    }
+
+}
---
+++ recycle-market-back/src/test/java/com/example/recyclemarketback/RecycleMarketBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recyclemarketback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleMarketBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
