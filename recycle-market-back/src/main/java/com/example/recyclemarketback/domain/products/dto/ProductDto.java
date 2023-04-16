698745458f2cddd66fd5594896f93f7a70ebff0b
Jeonwoojae <dnwo0719@icloud.com>
2023-04-16 17:10:13 +0900

    [fix]server: ProductDto에 sellerPhoneNumber 추가

--- recycle-market-back/src/main/java/com/example/recyclemarketback/domain/products/dto/ProductDto.java
+++ recycle-market-back/src/main/java/com/example/recyclemarketback/domain/products/dto/ProductDto.java
@@ -45,5 +45,6 @@ public class ProductDto {
         this.startPrice = entity.getStartPrice();
         this.currentPrice = entity.getCurrentPrice();
         this.productState = entity.getProductState();
+        this.sellerPhoneNumber = entity.getSeller().getPhoneNumber();
     }
 }
