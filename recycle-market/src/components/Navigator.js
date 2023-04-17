e0cae2fbcb95c245db5025d09019d66249173b1b
Jeonwoojae <dnwo0719@icloud.com>
2023-04-17 17:39:14 +0900

    [fix]:네비게이터 내거래, 내입찰 버튼 추가

--- recycle-market/src/components/Navigator.js
+++ recycle-market/src/components/Navigator.js
@@ -1,35 +1,57 @@
-import { AppBar, Button, Grid, Toolbar, Typography } from '@material-ui/core'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { signout } from '../service/ApiService'
+import { AppBar, Button, Grid, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
+import { signout } from "../service/ApiService";
 
 function Navigator() {
   const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  const role = localStorage.getItem("ROLE");
 
   return (
-      // navigationBar
+    // navigationBar
     <AppBar position="static">
       <Toolbar>
-        <Grid justifyContent="space-between" container>
-          <Grid item><div style={{cursor:"pointer"}} onClick={()=>window.location.href="/"}>
-          <Typography variant="h6">Recycle-market</Typography>
-          </div>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <div
+              style={{ cursor: "pointer" }}
+              onClick={() => (window.location.href = "/")}
+            >
+              <Typography variant="h6">Recycle-market</Typography>
+            </div>
           </Grid>
           <Grid item>
-          <Button color="inherit" href="/profile">
-              my
-            </Button>
-            {accessToken != null? (<Button color="inherit" onClick={signout}>
-              logout
-            </Button>):(<Button color="inherit" href="/login">
-              login
-            </Button>)}
-            
+            {accessToken ? (
+              <>
+                <Button color="inherit" component={Link} to="/profile">
+                  프로필
+                </Button>
+                <Button color="inherit" component={Link} to="/transaction">
+                  내 거래
+                </Button>
+                {role == "BUYER" ? (
+                  <Button color="inherit" component={Link} to="/bid">
+                    내 입찰
+                  </Button>
+                ) : (
+                  <></>
+                )}
+                <Button color="inherit" onClick={signout}>
+                  logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button color="inherit" component={Link} to="/login">
+                  login
+                </Button>
+              </>
+            )}
           </Grid>
         </Grid>
       </Toolbar>
     </AppBar>
-  )
+  );
 }
 
-export default Navigator
+export default Navigator;
